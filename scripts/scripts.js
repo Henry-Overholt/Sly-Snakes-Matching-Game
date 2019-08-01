731f9af9f3458b2f73463c35b34a6fea3144fee4
alicia <alicia.lecznar@gmail.com>
2019-08-01 14:09:23 -0400

    starter flip

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+function flip(event){
+	let element = event.currentTarget;
+	if (element.className === "card") {
+    if(element.style.transform == "rotateY(180deg)") {
+      element.style.transform = "rotateY(0deg)";
+    }
+    else {
+      element.style.transform = "rotateY(180deg)";
+    }
+  }
+};
